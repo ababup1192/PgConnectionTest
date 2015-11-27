105fee2aa3b1040bb5e502b5226c0845482d8886
ababup1192 <ababup1192@gmail.com>
2015-11-27 19:42:45 +0900

    Using jetty factory

--- src/main/java/org/ababup1192/Main.java
+++ src/main/java/org/ababup1192/Main.java
@@ -10,20 +10,31 @@ public class Main {
 
     public Main() {
         try {
-            // Naming(JNDI)を管理するためのファクトリをMockで用意する。
+            // JNDIのFactoryをjettyさんから借りる。
             System.setProperty(Context.INITIAL_CONTEXT_FACTORY,
-                    "org.ababup1192.MyInitialContextFactory");
+                    "org.eclipse.jetty.jndi.InitialContextFactory");
+            System.setProperty(Context.URL_PKG_PREFIXES, "org.eclipse.jetty.jndi");
+
+            String dataSourceName = "java:/comp/env/jdbc/postgres";
 
             PGPoolingDataSource poolingDataSource = new PGPoolingDataSource();
-            poolingDataSource.setDataSourceName("java:comp/env/jdbc/database");
+            poolingDataSource.setDataSourceName(dataSourceName);
             poolingDataSource.setServerName("localhost");
             poolingDataSource.setPortNumber(5432);
             poolingDataSource.setDatabaseName("vagrant");
             poolingDataSource.setUser("vagrant");
             poolingDataSource.setPassword("vagrant");
-            new InitialContext().rebind("java:comp/env/jdbc/database", poolingDataSource);
 
-            System.out.println("ぬけた！");
+            // dsを保存するためのディレクトリを作っていく。
+            InitialContext context = new InitialContext();
+            context.createSubcontext("jdbc");
+            context.createSubcontext("jdbc/postgres");
+            // ds1という名前でファイル(オブジェクト)を格納。
+            context.rebind("jdbc/postgres/ds1", poolingDataSource);
+
+            // 格納した名前で取り出してみる。
+            PGPoolingDataSource dataSource = InitialContext.doLookup("jdbc/postgres/ds1");
+            System.out.println(dataSource);
         } catch (NamingException e) {
             e.printStackTrace();
         }
