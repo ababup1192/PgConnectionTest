8007185b946c4f3cc807229f9c7d494cefb55a3c
ababup1192 <ababup1192@gmail.com>
2015-11-27 17:26:49 +0900

    first commit

---
+++ src/main/java/org/ababup1192/Main.java
@@ -0,0 +1,12 @@
+package org.ababup1192;
+
+public class Main {
+
+    public Main(){
+        System.out.println("Hello");
+    }
+
+    public static void main(String args[]){
+     new Main();
+    }
+}
